16277740fc17615f6f82808c36cecfcedc235197
unknown <danielsolis023@gmail.com>
2021-05-19 18:17:02 -0500

    Se cambian textos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,20 +19,20 @@
                 <div class="row justify-content-center h-100">
                     <div class="col xl-5 col-lg-5 col-md-3 col-sm-9">
                         <h2>¿POR QUÉ ELEGIRNOS A <span>NOSOTROS</span>?</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aspernatur, voluptas alias ipsam ea dolorem vel corporis quo ab dolor recusandae aliquid, similique impedit exercitationem repellat maiores quam, nesciunt sequi!</p>
+                        <p>Nos enfocamos en formar traders profesionales; contamos con las herramientas específicas para tu crecimiento; la eficiencia de tu aprendizaje es nuestra prioridad; reducimos la curva de aprendizaje un 50%.</p>
                     </div>
                     <div class="col-xl-1 col-lg-1 d-lg-block d-md-none"></div>
                     <div class="col-xl-2 col-lg-2 col-md-3 col-sm-9 col-col-2 content-why-choose-img">
                         <img src="{{ asset('public/img/Iconos/ayuda.svg') }}" class="img-why-choose">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae architecto ipsum delectus corrupti eveniet nemo, nisi velit id.</p>
+                        <p>Hemos contribuido en el desarrollo de traders financiados, gestores de cuentas y fondos de inversión profesionales.</p>
                     </div>
                     <div class="col-xl-2 col-lg-2 col-md-3 col-sm-9 col-col-2 content-why-choose-img">
                         <img src="{{ asset('public/img/Iconos/equipo.svg') }}" class="img-why-choose">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae architecto ipsum delectus corrupti eveniet nemo.</p>
+                        <p>Somos traders y educadores profesionales en mercados de futuros, CFD, cryptos y tecnología blockchain.</p>
                     </div>
                     <div class="col-xl-2 col-lg-2 col-md-3 col-sm-9 col-col-2 content-why-choose-img">
                         <img src="{{ asset('public/img/Iconos/crecimiento.svg') }}" class="img-why-choose">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae.</p>
+                        <p>Relación costo beneficio, vas a conseguir información de calidad con un costo menor al del mercado. Toda la información en un mismo lugar, sin costos adicionales y sesiones en vivo toda la semana.</p>
                     </div>
                 </div>
             </div>
@@ -45,9 +45,9 @@
                     </div>
                     <div class="col-xl-7 col-lg-7 col-md-7 -col-sm-9 col-12 content-text-what-is">
                         <h2>¿QUÉ ES MINDS <span>O</span>VER MARKET?</h2>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, modi natus. Rerum minus ea.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae repudiandae veniam atque, recusandae nihil quisquam sint! Ducimus, itaque? Quo tempora.</p>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, modi natus. Rerum minus ea.</p>
+                        <p>Somos una escuela de trading e inversiones fundada en el 2018</p>
+                        <p> contamos con educadores profesionales para guiarte en el desarrollo de tu carrera como trader, nuestros servicios están estratégicamente planeados para brindarte educación y experiencia que te llevarán hacia la consistencia a largo plazo.</p>
+                        <p>Nuestro propósito es formar mentes profesionales del trading.</p>
                     </div>
                 </div>
             </div>
@@ -59,12 +59,12 @@
                         <div class="card card-membership card-top" style="width: 18rem;">
                             <div class="card-header header-green">
                                 <div class="row">
-                                    <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 type-description-membership">
-                                        <h6>TRADER CFDS</h6>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 type-description-membership text-center">
+                                        <h6>Membresía mensual </h6>
                                     </div>
-                                    <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12  text-center price-description-membership">
-                                        <h3 class="price-membership"><span>$</span>349</h3>
-                                        <p>CDFS, CFDS, CFDS y CFDS</p>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12  text-center price-description-membership">
+                                        <h3 class="price-membership"><span>USD</span> 50</h3>
+                                        <!-- <p>CDFS, CFDS, CFDS y CFDS</p> -->
                                     </div>
                                 </div>
                             </div>
@@ -72,15 +72,13 @@
                                 <div class="row">
                                     <div class="col-12 d-flex justify-content-center list-contain">
                                         <ul class="list-inline">
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
+                                            <li>Trading en Vivo</li>
+                                            <li>Trading Academy</li>
+                                            <li>WeProfit</li>
+                                            <li>Criptoanálisis</li>
+                                            <li>Podcast (Markets)</li>
+                                            <li>Mindset</li>
+                                            <li>Grupos de Telegram.</li>
                                         </ul>
                                     </div>
                                     <div class="col-12 d-flex justify-content-center">
@@ -95,9 +93,9 @@
                             <div class="card-header header-purple">
                                 <div class="row">
                                     <div class="col-12 text-center price-description-membership">
-                                        <h6>INVERSIONISTA MAESTRO</h6>
-                                        <h3 class="price-membership"><span>$</span>499</h3>
-                                        <p>CDFS, CFDS, CFDS y CFDS</p>
+                                        <h6>Membresía semestral</h6>
+                                        <h3 class="price-membership"><span>USD</span> 250</h3>
+                                        <p>(Te ahorras 1 membresía)</p>
                                     </div>
                                 </div>
                             </div>
@@ -105,15 +103,13 @@
                                 <div class="row">
                                     <div class="col-12 d-flex justify-content-center list-contain">
                                         <ul class="list-inline">
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
+                                        <li>Trading en Vivo</li>
+                                            <li>Trading Academy</li>
+                                            <li>WeProfit</li>
+                                            <li>Criptoanálisis</li>
+                                            <li>Podcast (Markets)</li>
+                                            <li>Mindset</li>
+                                            <li>Grupos de Telegram.</li>
                                         </ul>
                                     </div>
                                     <div class="col-12 d-flex justify-content-center">
@@ -127,12 +123,12 @@
                         <div class="card card-membership card-top" style="width: 18rem;">
                             <div class="card-header header-green">
                                 <div class="row">
-                                    <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12 type-description-membership">
-                                        <h6>TRADER CFDS</h6>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 type-description-membership text-center">
+                                        <h6>Anualidad</h6>
                                     </div>
-                                    <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 col-12 text-center price-description-membership">
-                                        <h3 class="price-membership"><span>$</span>349</h3>
-                                        <p>CDFS, CFDS, CFDS y CFDS</p>
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center price-description-membership">
+                                        <h3 class="price-membership"><span>USD</span> 500</h3>
+                                        <p>(te ahorras 2 membresías)</p>
                                     </div>
                                 </div>
                             </div>
@@ -140,15 +136,13 @@
                                 <div class="row">
                                     <div class="col-12 d-flex justify-content-center list-contain">
                                         <ul class="list-inline">
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
-                                            <li>Lorem ipsum dolor, sit amet.</li>
+                                        <li>Trading en Vivo</li>
+                                            <li>Trading Academy</li>
+                                            <li>WeProfit</li>
+                                            <li>Criptoanálisis</li>
+                                            <li>Podcast (Markets)</li>
+                                            <li>Mindset</li>
+                                            <li>Grupos de Telegram.</li>
                                         </ul>
                                     </div>
                                     <div class="col-12 d-flex justify-content-center">
@@ -185,7 +179,7 @@
                             </ul>
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <h6>DANIEL PEÑA</h6>
+                                    <h6>Manuel Abadía</h6>
                                     <ul class="list-inline star-rating">
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
@@ -193,10 +187,10 @@
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                     </ul>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sed quibusdam saepe exercitationem expedita ratione, nemo deserunt omnis aut numquam.</p>
+                                    <p>Minds Over Market, Me Ayudo Muchísimo, A Lograr Entender, Como Los Mercados Bursátiles Funcionan, Cuando Inicie Tenia Ideas Erróneas Sobre El Mercado, Pero En Mi Proceso En Minds, Con Esfuerzo, Tiempo Y Mentoría De Isaac, He Logrado Encontrar, Mi Propia Manera De Hacer Trading.</p>
                                 </div>
                                 <div class="carousel-item">
-                                    <h6>DANIEL PEÑA</h6>
+                                    <h6>Daniel Peña</h6>
                                     <ul class="list-inline star-rating">
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
@@ -204,10 +198,10 @@
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                     </ul>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sed quibusdam saepe exercitationem expedita ratione, nemo deserunt omnis aut numquam.</p>
+                                    <p>Estoy Agradecido Por Que Siempre Estas Para Apoyarnos, Dedicas Mucho Tiempo A Nosotros Y Eso Es Algo Bueno, Tienes Demasiados Conocimientos Para Brindarnos Y Nos Motivas Día A Día</p>
                                 </div>
                                 <div class="carousel-item">
-                                    <h6>DANIEL PEÑA</h6>
+                                    <h6>Josue Alvarez</h6>
                                     <ul class="list-inline star-rating">
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
@@ -215,10 +209,10 @@
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                     </ul>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sed quibusdam saepe exercitationem expedita ratione, nemo deserunt omnis aut numquam.</p>
+                                    <p>Estoy Agradecido Por Que Siempre Estas Para Apoyarnos, Dedicas Mucho Tiempo A Nosotros Y Eso Es Algo Bueno, Tienes Demasiados Conocimientos Para Brindarnos Y Nos Motivas Día A Día</p>
                                 </div>
                                 <div class="carousel-item">
-                                    <h6>DANIEL PEÑA</h6>
+                                    <h6>Rudy Alvarado</h6>
                                     <ul class="list-inline star-rating">
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
@@ -226,7 +220,18 @@
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                         <li class="list-inline-item"><i class="fas fa-star"></i></li>
                                     </ul>
-                                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis sed quibusdam saepe exercitationem expedita ratione, nemo deserunt omnis aut numquam.</p>
+                                    <p>Máster Quiero Agradecerte El Gran Apoyo Que Das A La Comunidad De MINDS OVER MARKET, Que Para Mi Es Una Gran Familia Que Poco A Poco Crece, No Veo Otra Comunidad Como La FAMILIA MINDS Que Se Empeñan En Enseñar Sin Trucos, Y Mucho Menos Falsas Promesas, Porque De Eso Hay Mucho. Estar En MINDS OVER MARKET Es La Mejor Decisión Que Puede Tomar Cualquiera Que Desee Estar En El Mundo Del Trading.</p>
+                                </div>
+                                <div class="carousel-item">
+                                    <h6>Serch Jacobo</h6>
+                                    <ul class="list-inline star-rating">
+                                        <li class="list-inline-item"><i class="fas fa-star"></i></li>
+                                        <li class="list-inline-item"><i class="fas fa-star"></i></li>
+                                        <li class="list-inline-item"><i class="fas fa-star"></i></li>
+                                        <li class="list-inline-item"><i class="fas fa-star"></i></li>
+                                        <li class="list-inline-item"><i class="fas fa-star"></i></li>
+                                    </ul>
+                                    <p>Aprender En Cualquier Lado, Vivir El Trading Solo En Minds Over Markets, Mas Que Recomendado.</p>
                                 </div>
                             </div>
                             {{-- <a class="carousel-control-prev" href="#demo" data-slide="prev">
